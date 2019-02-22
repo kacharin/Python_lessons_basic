d2a5b1e1503358df4a6a69381ab45eb62b392776
Mikhail Kacharin <47451806+kacharin@users.noreply.github.com>
2019-02-22 16:58:45 +0300

    Качарин М.А. Домашка. Урок 03.
    Легкий вариант.

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,14 +4,46 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+"""
+
 def my_round(number, ndigits):
-    pass
+    number = str(number)
+    ndigits = int(ndigits)
+    
+    # Находим место где целая часть отделяется от дробной
+    p = number.find('.')
+    
+    # Создаем число на которое нужно увеличить
+    # в случае округления в большую сторону
+    okrup = str(0.)
+    i = 0
+    while i < ndigits-2:
+        okrup = okrup + str(0)
+        i += 1
+    okrup = okrup + str(1)
+    
+    # Находим цифру, которая определяет куда округлять
+    lastdigit = number[ndigits+p+1]
+    
+    if int(lastdigit) > 5:
+        # Округление в большую сторону
+        number = float(number) + float(okrup)
+        number = str(number)
+        number = number[0:ndigits+p+1]
+    else:
+        # Округление в меньшую сторону
+        number = str(number)
+        number = number[0:ndigits+p+1]
+    return number
 
-
+# Это все округляется в большую сторону
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
+# Добавим пример с округлением в меньшую
+print(my_round(235232.987654321, 7))
 
+"""
 
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
@@ -19,10 +51,36 @@ print(my_round(2.9999967, 5))
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
+"""
+
 def lucky_ticket(ticket_number):
-    pass
+    ticket_number = str(ticket_number)
+    
+    sum_begin = 0
+    sum_end = 0
+    
+    if len(ticket_number) == 6:
+        i = 0
+        while i < 6:
+            if i < 3:
+                sum_begin = sum_begin + int(ticket_number[i])
+            else:
+                sum_end = sum_end + int(ticket_number[i])
+            i += 1
+        if sum_begin == sum_end:
+            lucky_ticket = 'Ваш билет счастливый. Поздравляем!'
+        else:
+            lucky_ticket = 'К сожалению, билет не счастливый.'
+    else:
+        lucky_ticket = 'Неправильный билет. Номер должен состоять из 6 цифр.'
+    return lucky_ticket
 
 
+# Это примеры счастливого и неправильного билетов
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+# Добавим пример не счастливого билета
+print(lucky_ticket(478682))
+
+"""

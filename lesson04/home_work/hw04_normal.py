c7a3c7add8002a0922cb54e3c65d19f3abcfe33b
Mikhail Kacharin <47451806+kacharin@users.noreply.github.com>
2019-02-26 11:08:11 +0300

    ДЗ 4 средний
    Готово!

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -20,6 +20,29 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+"""
+
+#Используем re
+import re
+lowercase = [i for i in re.findall('([a-z]+)[A-Z]', line)]
+print('Используем re:')
+print(lowercase)
+print()
+
+#Не используем re
+lowercase2 = []
+lc2 = ''
+for s in range(len(line)):
+    if line[s].islower():
+        lc2 += line[s]
+    else:
+        if line[s - 1].islower():
+            lowercase2.append(lc2)
+            lc2 = ''
+print('Не используем re:')
+print(lowercase2)
+
+"""
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -28,6 +51,8 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+
+
 
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
@@ -45,9 +70,71 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+"""
+
+#Используем re
+import re
+uppercase = [i for i in re.findall('[a-z][a-z]([A-Z]+)[A-Z][A-Z]', line_2)]
+print('Используем re:')
+print(uppercase)
+print()
+
+#Не используем re
+uppercase2 = []
+uc2 = ''
+
+for s in range(len(line_2)):
+    try:
+        if (line_2[s - 2].islower() and line_2[s - 1].islower() and line_2[s].isupper()) or len(
+                uc2) > 0:
+            uc2 += line_2[s]
+            if line_2[s + 1].isupper() and line_2[s + 2].isupper():
+                if line_2[s + 2].isupper() and line_2[s + 3].isupper():
+                    pass
+                else:
+                    uppercase2.append(uc2)
+                    uc2 = ''
+            else:
+                uc2 = ''
+    except IndexError:
+        pass
+print('Не используем re:')
+print (uppercase2)
+
+"""
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+"""
+
+from random import randint
+
+with open('newfile.txt', 'w', encoding='utf-8') as nf:
+    n = [str(random.randint(0, 9)) for _ in range(2499)]
+    n = str(random.randint(1, 9)) + ''.join(n)
+    nf.write(n)
+
+with open('newfile.txt', 'r', encoding='utf-8') as nf:
+    number = nf.readline()
+    search = '([0]{2,}|[1]{2,}|[2]{2,}|[3]{2,}|[4]{2,}|' \
+              '[5]{2,}|[6]{2,}|[7]{2,}|[8]{2,}|[9]{2,})'
+    sequence = re.findall(search, number)
+
+print('2500-значное произвольное число:')
+print(number)
+print()
+print('Все последовательности одинаковых цифр:')
+print(sequence)
+print()
+
+longest = []
+[longest.insert(0, i) for i in sequence if len(i) > len(longest)]
+print('Самая длинная последовательность одинаковых цифр:')
+print(longest[0])
+
+"""
